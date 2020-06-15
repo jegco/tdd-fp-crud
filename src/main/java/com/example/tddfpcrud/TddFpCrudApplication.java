a59aa5d8ccfa6ac218417e31913f0a35b9db055b
Jorge Eliecer Caro Olmos <jorge.caro@PSOFKA0225.local>
2020-06-15 16:51:50 -0500

    initial commit

---
+++ src/main/java/com/example/tddfpcrud/TddFpCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddfpcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddFpCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddFpCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddfpcrud/TddFpCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddfpcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddFpCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
